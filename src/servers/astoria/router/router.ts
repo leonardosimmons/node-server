663e17c82517c324300efa08824b982f6ff9fa48
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-06-14 09:57:28 -0700

    added cart route

--- src/servers/astoria/router/router.ts
+++ src/servers/astoria/router/router.ts
@@ -4,16 +4,19 @@ import userRoutes from './routes/user';
 import navbarRoutes from './routes/navbar';
 import indexPageRoutes from './routes/index-page';
 import productRoutes from './routes/products';
+import cartRoutes from './routes/cart';
 
 const router: Express.Router = Express.Router();
 
-router.use('/user', userRoutes);
-
-router.use('/navbar', navbarRoutes);
+router.use('/cart', cartRoutes);
 
 router.use('/index', indexPageRoutes);
 
+router.use('/navbar', navbarRoutes);
+
 router.use('/product', productRoutes);
+
+router.use('/user', userRoutes);
 
 
 export default router;
