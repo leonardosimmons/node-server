10789e5d26b3d60d66c95711334963662ee236c9
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-06-18 14:40:08 -0700

    added jwt token

--- src/servers/astoria/controllers/user.ts
+++ src/servers/astoria/controllers/user.ts
@@ -1,47 +1,53 @@
 
 import Express from 'express';
-import { httpError } from '../../../helpers/functions';
-import { UserController } from '../models/User';
+import { AccessToken } from '../../../utils/types';
 import { NewUserToken, User, UserTableData } from '../utils/types';
+
+import { UserController } from '../models/User';
+import { generateAccessToken, httpError } from '../../../helpers/functions';
 
 
 export async function add(req: Express.Request, res: Express.Response, next: Express.NextFunction): Promise<void>
 {
   try {
+    if(!req.body) {
+      res.statusCode = 404;
+      res.end('Error');
+      return;
+    }
+
     const cntrl: UserController = new UserController();
 
-    if (req.body) {
-      const name: string = req.body.name;
-      const email: string = req.body.email;
-      const image: string = req.body.image;
+    const name: string = req.body.name;
+    const email: string = req.body.email;
+    const image: string = req.body.image;
 
-      await cntrl.generateId()
-      .then((n: number) => {
-        const id: number = n
+    await cntrl.generateId()
+    .then((n: number) => {
+      const id: number = n
+      
+      if (name) {
+        const newUser: NewUserToken = {id, name, email, image};
+
+        cntrl.create(newUser);
         
-        if (name) {
-          const newUser: NewUserToken = {id, name, email, image};
-  
-          cntrl.create(newUser);
-          
-          return newUser;
-        }
-      })
-      .then((user) => {
-        res.status(201).json({
-          message: 'User successfully added to the database',
-          payload: {
-            id: user?.id as number,
-            info: {
-              name: user?.name as string,
-              email: user?.email as string,
-              image: user?.image as string
-            }
+        return newUser;
+      }
+    })
+    .then((user) => {
+      res.status(201).json({
+        message: 'User successfully added to the database',
+        payload: {
+          id: user?.id as number,
+          info: {
+            name: user?.name as string,
+            email: user?.email as string,
+            image: user?.image as string
           }
-        });
-      })
-      .catch(err => { throw new Error(err)}) 
-    }
+        }
+      });
+    })
+    .catch(err => { throw new Error(err)}) 
   }
   catch(err) {
     const msg: string = 'Unable to add user to database';
@@ -84,19 +90,28 @@ export async function get(req: Express.Request, res: Express.Response, next: Exp
 export async function signIn(req: Express.Request, res: Express.Response, next: Express.NextFunction): Promise<void>
 {
   try {
+    if(!req.body) {
+      res.statusCode = 404;
+      res.end('Error');
+      return;
+    }
+
     const cntrl: UserController = new UserController();
 
-    if(req.body) {
-      const id: string = req.body.u_Id as string;
-      const col: string = 'signed_in';
+    const u_id: string = req.body.u_id as string;
+    const col: string = 'signed_in';
 
-      cntrl.update(parseInt(id), col, 1);
+    await cntrl.update(parseInt(u_id), col, 1);
+    const token: AccessToken = generateAccessToken({u_id}, '60s');
 
-      res.status(200).json({
-        message: 'Success',
-        payload: true
-      });
-    }
+    res.status(200).json({
+      message: 'Success',
+      payload: {
+        u_id,
+        token,
+        signedIn: true
+      }
+    });
   }
   catch(err) {
     const msg: string = 'Unable to sign in user';
@@ -107,19 +122,23 @@ export async function signIn(req: Express.Request, res: Express.Response, next: 
 export async function signOut(req: Express.Request, res: Express.Response, next: Express.NextFunction): Promise<void>
 {
   try {
+    if(!req.body) {
+      res.statusCode = 404;
+      res.end('Error');
+      return;
+    }
+
     const cntrl: UserController = new UserController();
 
-    if(req.body) {
-      const id: string = req.body.u_Id as string;
-      const col: string = 'signed_in';
+    const id: string = req.body.u_id as string;
+    const col: string = 'signed_in';
 
-      cntrl.update(parseInt(id), col, 0);
+    await cntrl.update(parseInt(id), col, 0);
 
-      res.status(200).json({
-        message: 'success',
-        payload: false
-      });
-    }
+    res.status(200).json({
+      message: 'success',
+      payload: true
+    });
   }
   catch(err) {
     const msg: string = 'Unable to sign out user';
