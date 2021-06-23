40d27b16f2d041b3317632b7f2c844bac1963188
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-06-23 08:17:04 -0700

    updated

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -42,6 +42,10 @@ export function generateRandHash(bytes?: number): string {
   return require('crypto').randomBytes(bytes ? bytes : 64).toString('hex');
 };
 
+function getUniqueListBy(arr: any, key: any) {
+  return [...new Map(arr.map((item: any) => [item[key], item])).values()]
+}
+
 /**
  * Handles http errors
  * @param err 
@@ -75,4 +79,26 @@ export function preg_match(regex: string, c: Combinable): boolean | undefined {
  */
 export function randNum(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min)) + min;
+};
+
+
+export function removeDuplicateByVal(arr: Array<any>, val: any) {
+  return arr.reduce((unique: any, o) => {
+    if(!unique.some((obj: any)=> obj.order.id === o.order.id)) {
+      unique.push(o);
+    }
+    return unique;
+  },[]);
+};
+
+export function removeDuplicates(arr: any) {
+  let seen: any = {};
+  let ret_arr = [];
+  for (let i = 0; i < arr.length; i++) {
+      if (!(arr[i] in seen)) {
+          ret_arr.push(arr[i]);
+          seen[arr[i]] = true;
+      }
+  }
+  return ret_arr;
 };
