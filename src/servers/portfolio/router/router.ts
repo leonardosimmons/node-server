d90bc42fa4d4e77781a6dea89c777bc0a9f64160
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-07-09 17:44:24 -0700

    added

---
+++ src/servers/portfolio/router/router.ts
@@ -0,0 +1,48 @@
+
+import Express from 'express';
+
+import nodemailer from 'nodemailer';
+const sendGridTransport = require('nodemailer-sendgrid-transport');
+
+const router: Express.Router = Express.Router();
+
+const transporter = nodemailer.createTransport(sendGridTransport({
+  auth: {
+    api_key: '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'
+  }
+}));
+
+
+router.use('/contact-me-form-submit', (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+  if (req.body) {
+    const fn: string = req.body.firstname;
+    const ln: string = req.body.lastname;
+    const sub: string = req.body.subject;
+    const email: string = req.body.email;
+    const msg: string = req.body.message;
+
+    if (msg) {
+      transporter.sendMail({
+        to: process.env.EMAIL_SENDER as string,
+        from: process.env.EMAIL_SENDER as string,
+        subject: sub,
+        html: `
+          <h1>New Contact\n</h1>
+          <ul>
+            <li><p>first name: ${fn}</p></li>
+            <li><p>last name: ${ln}</p></li>
+            <li><p>subject: ${sub}</p></li>
+            <li><p>email: ${email}</p></li>
+            <li><p>message: ${msg}</p></li>
+          </ul>
+        `
+      });
+    }
+
+    res.status(200).json({
+      message: 'Success'
+    });
+  }
+});
+
+export default router;
