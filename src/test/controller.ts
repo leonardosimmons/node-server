1c7d2778269ace2081f85e7842ac9f46d875f0a9
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-07-12 15:41:18 -0700

    added

---
+++ src/test/controller.ts
@@ -0,0 +1,28 @@
+
+import Express from 'express';
+import { HttpError } from '../utils/types';
+
+import { CartController } from '../servers/astoria/models/Cart';
+import { randNum } from '../helpers/functions';
+
+
+export async function test(req: Express.Request, res: Express.Response, next: Express.NextFunction) 
+{
+  try {
+    const min: number = 100000000;
+    const max: number = 999999999;
+
+    const num: number = randNum(min, max);
+
+    res.status(200).json({
+      message: 'Success',
+      payload: num
+    });
+  }
+  catch(err) {
+    const error: HttpError = err;
+    error.statusCode = 502;
+    error.message = 'test failed';
+    next(err);
+  }
+};
