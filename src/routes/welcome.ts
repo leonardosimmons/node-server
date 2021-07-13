f3b6ca83d6d12b3b1e1749b3a51944d2ad901a83
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-07-12 21:17:00 -0700

    testing

--- src/routes/welcome.ts
+++ src/routes/welcome.ts
@@ -1,14 +1,28 @@
 
-import Express from 'express';
+import Express, { NextFunction } from 'express';
+import { randNum } from '../helpers/functions';
+import { HttpError } from '../utils/types';
 
 const router: Express.Router = Express.Router();
 
-router.use('/', (_: Express.Request, res: Express.Response) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>Easy Company</title></head>');
-  res.write('<body><h1>Test Text</h1></body>')
-  res.write('</html>');
+router.use('/', (_: Express.Request, res: Express.Response, next: NextFunction) => {
+  try {
+    const min: number = 100000000;
+    const max: number = 999999999;
+
+    const num: number = randNum(min, max);
+
+    res.status(200).json({
+      message: 'Success',
+      payload: num
+    });
+  }
+  catch(err) {
+    const error: HttpError = err;
+    error.statusCode = 502;
+    error.message = 'test failed';
+    next(err);
+  }
 });
 
 export default router;
