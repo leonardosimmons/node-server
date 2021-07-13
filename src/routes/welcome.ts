1302e2fa1b1e42bbb377e83e0baafb7bca0269cb
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-07-12 19:51:22 -0700

    test

--- src/routes/welcome.ts
+++ src/routes/welcome.ts
@@ -7,7 +7,7 @@ router.use('/', (_: Express.Request, res: Express.Response) => {
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>Easy Company</title></head>');
-  res.write('<body><h1>Welcome to the Easy Company node server</h1></body>')
+  res.write('<body><h1>Test Text</h1></body>')
   res.write('</html>');
 });
 
